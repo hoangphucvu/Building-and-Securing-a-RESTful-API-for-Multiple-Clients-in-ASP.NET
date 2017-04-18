3f46c6edf350c4058fc0363bfdd9c4c82c5bc020
ngohungphuc <ngohungphuc95@gmail.com>
2017-04-18 14:27:59 +0700

    Logger

--- RestAPI/Library.API/Controllers/BooksController.cs
+++ RestAPI/Library.API/Controllers/BooksController.cs
@@ -9,6 +9,7 @@ using Library.API.Models;
 using Library.API.Services;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace Library.API.Controllers
 {
@@ -16,10 +17,12 @@ namespace Library.API.Controllers
     public class BooksController : Controller
     {
         private ILibraryRepository _libraryRepository;
+        private ILogger<BooksController> _logger;
 
-        public BooksController(ILibraryRepository libraryRepository)
+        public BooksController(ILibraryRepository libraryRepository, ILogger<BooksController> logger)
         {
             _libraryRepository = libraryRepository;
+            _logger = logger;
         }
 
         [HttpGet]
@@ -115,6 +118,7 @@ namespace Library.API.Controllers
                 throw new Exception($"Delete a book for author {authorId} failed on save");
             }
 
+            _logger.LogInformation(100, $"Book {id} for author {authorId} was deleted");
             return NoContent();
         }
 
@@ -222,7 +226,9 @@ namespace Library.API.Controllers
 
             var bookToPatch = Mapper.Map<BookForUpdateDto>(bookForAuthorFromRepo);
 
-            patchDoc.ApplyTo(bookToPatch, ModelState);
+            //patchDoc.ApplyTo(bookToPatch, ModelState);
+
+            patchDoc.ApplyTo(bookToPatch);
 
             if (bookToPatch.Description == bookToPatch.Title)
             {
